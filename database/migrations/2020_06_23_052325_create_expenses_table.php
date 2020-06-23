d007779857de43b54b45e26909e0c1471cffd73a
PetersonJFP <peterson.jfp@gmail.com>
2020-06-23 02:52:01 -0300

    Add initial models

---
+++ database/migrations/2020_06_23_052325_create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->float('amount', 8, 2);
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->bigInteger('expense_category_id')->unsigned();
+
+            $table->foreign('expense_category_id')->references('id')->on('expense_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
---
+++ database/seeds/ExpenseCategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\ExpenseCategory;
+
+class ExpenseCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Alimentação', 'Transporte', 'Farmácia', 'Despesas Fixas', 'Pecuária', 'Estudo', 'Lazer'];
+        foreach ($categories as $category) {
+            $cat = new ExpenseCategory();
+            $cat->name = $category;
+            $cat->save();
+        }
+    }
+}
